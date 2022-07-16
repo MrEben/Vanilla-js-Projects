4c627ccc87271c786010a5535153d429b8cdd33a
Ebenezer <aniagyeiodame224@gmail.com>
2022-07-16 00:16:46 +0000

    updated

--- Countdown Project/script.js
+++ Countdown Project/script.js
@@ -24,12 +24,20 @@ const weekdays = [
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.daedline');
 const items = document.querySelectorAll('.deadline-format h4');
-const date = new Date(2022, 6, 12, 10, 5);
-console.log(date);
+const date = new Date(2022, 7, 12, 10, 5);
 const year = date.getFullYear();
 const hour = date.getHours();
 const minutes = date.getMinutes()
 const month = date.getMonth();
 const weekday = date.getDay();
 const dates = date.getDate();
-giveaway.textContent = `giveaway ends on ${weekdays[weekday]},${dates} ${months[month]} ${year} ${hour}:0${minutes}am`
+giveaway.textContent = `giveaway ends on ${weekdays[weekday]},${dates} ${months[month]} ${year} ${hour}:0${minutes}am`;
+const futureTime=date.getTime();
+function getRemainingTime() {
+  const today=new Date().getTime();
+  const t=date-today;
+  console.log(t);
+  const oneDay=24*60*60*1000;
+  console.log(oneDay);
+};
+getRemainingTime();
